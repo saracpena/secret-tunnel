2cbaf69c9b126636f7bb91fc373c068ebdea2f87
Sara <s.cristal.p@gmail.com>
2026-07-07 20:28:33 +0200

    complete entrance

--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,6 +1,20 @@
 /** Users can enter their name to receive a token from the API. */
+import { useAuth } from "./AuthContext";
+
 export default function Entrance() {
   // TODO: call signup when form is submitted
+
+  const { signup } = useAuth();
+  
+const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const username = formData.get("name");
+
+    await signup(username);
+  };
+
 
   return (
     <>
@@ -15,7 +29,7 @@ export default function Entrance() {
         fixed on you. The one on the left opens its mouth, and with a deep,
         rumbling voice, it asks, "Who approaches? Speak your name."
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Name
           <input name="name" />
